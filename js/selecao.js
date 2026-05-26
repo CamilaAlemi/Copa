49d0ccf10e5c10b2b0893d72b51aef32017588a6
Sofia Ornellas <vitorinosofia4@gmail.com>
2026-05-26 19:51:21 -0300

    Configurações pag pais

---
+++ js/selecao.js
@@ -0,0 +1,553 @@
+const selecoes = {
+  
+  
+  brasil: [
+
+  {codigo: "BRA - 012",
+ imagem: "../../imgs/Brasil/selecao/ederson_SB.jpg",
+    posicao: "GOLEIRO",
+    nome: "Ederson",
+    cidade: "Osasco, Brasil",
+    copas: "3ª",
+    idade: "32",
+    destaque: "Reflexo",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 023",
+ imagem: "../../imgs/Brasil/selecao/weverton_SB.jpg",
+    posicao: "GOLEIRO",
+    nome: "Weverton",
+    cidade: "Rio Branco, Brasil",
+    copas: "2ª",
+    idade: "38",
+    destaque: "Experiência",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+{
+    codigo: "BRA - 001",
+    imagem: "../../imgs/Brasil/selecao/alisson_SB.jpg",
+    posicao: "GOLEIRO",
+    nome: "Alisson",
+    cidade: "Novo Hamburgo, Brasil",
+    copas: "3ª",
+    idade: "33",
+    destaque: "Seguro",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+{
+    codigo: "BRA - 003",
+ imagem: "../../imgs/Brasil/selecao/bremer_SB.webp",
+    posicao: "ZAGUEIRO",
+    nome: "Bremer",
+    cidade: "Itapitanga, Brasil",
+    copas: "2ª",
+    idade: "29",
+    destaque: "Força",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 014",
+ imagem: "../../imgs/Brasil/selecao/gabrielMagalhaes_SB.webp",
+    posicao: "ZAGUEIRO",
+    nome: "Gabriel Magalhães",
+    cidade: "São Paulo, Brasil",
+    copas: "1ª",
+    idade: "28",
+    destaque: "Marcação",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 002",
+ imagem: "../../imgs/Brasil/selecao/danilo_SB.jpg",
+    posicao: "LATERAL",
+    nome: "Danilo",
+    cidade: "Bicas, Brasil",
+    copas: "3ª",
+    idade: "35",
+    destaque: "Liderança",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+{
+    codigo: "BRA - 004",
+    imagem: "../../imgs/Brasil/selecao/marquinhos.webp",
+    posicao: "ZAGUEIRO",
+    nome: "Marquinhos",
+    cidade: "São Paulo, Brasil",
+    copas: "3ª",
+    idade: "31",
+    destaque: "Capitão",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 013",
+    imagem: "../../imgs/Brasil/selecao/ibanez_SB.webp",
+    posicao: "ZAGUEIRO",
+    nome: "Ibañez",
+    cidade: "Canela, Brasil",
+    copas: "1ª",
+    idade: "27",
+    destaque: "Marcação",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 015",
+    imagem: "../../imgs/Brasil/selecao/leoPereira_SB.webp",
+    posicao: "ZAGUEIRO",
+    nome: "Léo Pereira",
+    cidade: "Curitiba, Brasil",
+    copas: "1ª",
+    idade: "30",
+    destaque: "Saída de bola",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 016",
+    imagem: "../../imgs/Brasil/selecao/douglasantos.webp",
+    posicao: "LATERAL",
+    nome: "Douglas Santos",
+    cidade: "João Pessoa, Brasil",
+    copas: "1ª",
+    idade: "31",
+    destaque: "Apoio",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 024",
+    imagem: "../../imgs/Brasil/selecao/wesley_SB.jpg",
+    posicao: "LATERAL",
+    nome: "Wesley",
+    cidade: "Açailândia, Brasil",
+    copas: "1ª",
+    idade: "22",
+    destaque: "Velocidade",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+{
+    codigo: "BRA - 025",
+    imagem: "../../imgs/Brasil/selecao/daniloSantosBotafogo_SB.jpg",
+    posicao: "VOLANTE",
+    nome: "Danilo Santos",
+    cidade: "Salvador, Brasil",
+    copas: "1ª",
+    idade: "24",
+    destaque: "Intensidade",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 026",
+    imagem: "../../imgs/Brasil/selecao/fabinho_SB.jpg",
+    posicao: "VOLANTE",
+    nome: "Fabinho",
+    cidade: "Campinas, Brasil",
+    copas: "2ª",
+    idade: "32",
+    destaque: "Equilíbrio",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 006",
+ imagem: "../../imgs/Brasil/selecao/alex_SB.jpg",
+    posicao: "LATERAL",
+    nome: "Alex Sandro",
+    cidade: "Catanduva, Brasil",
+    copas: "3ª",
+    idade: "35",
+    destaque: "Regularidade",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+{
+    codigo: "BRA - 027",
+    imagem: "../../imgs/Brasil/selecao/igorthiago_SB.webp",
+    posicao: "ATACANTE",
+    nome: "Igor Thiago",
+    cidade: "Gama, Brasil",
+    copas: "1ª",
+    idade: "24",
+    destaque: "Força física",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 007",
+    imagem: "../../imgs/Brasil/selecao/raphinha_SB.webp",
+    posicao: "PONTA",
+    nome: "Raphinha",
+    cidade: "Porto Alegre, Brasil",
+    copas: "2ª",
+    idade: "29",
+    destaque: "Velocidade",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 010",
+    imagem: "../../imgs/Brasil/selecao/viniJr_SB.webp",
+    posicao: "ATACANTE",
+    nome: "Vini Jr.",
+    cidade: "São Gonçalo, Brasil",
+    copas: "2ª",
+    idade: "25",
+    destaque: "Craque",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+{
+    codigo: "BRA - 005",
+ imagem: "../../imgs/Brasil/selecao/casemiro.webp",
+    posicao: "VOLANTE",
+    nome: "Casemiro",
+    cidade: "São José dos Campos, Brasil",
+    copas: "4ª",
+    idade: "34",
+    destaque: "Experiência",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 008",
+ imagem: "../../imgs/Brasil/selecao/brunoGuimaraes_SB.jpg",
+    posicao: "MEIA",
+    nome: "Bruno Guimarães",
+    cidade: "Rio de Janeiro, Brasil",
+    copas: "2ª",
+    idade: "29",
+    destaque: "Controle",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 017",
+ imagem: "../../imgs/Brasil/selecao/lucasPaqueta_SB.webp",
+    posicao: "MEIA",
+    nome: "Lucas Paquetá",
+    cidade: "Rio de Janeiro, Brasil",
+    copas: "3ª",
+    idade: "29",
+    destaque: "Criatividade",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 011",
+ imagem: "../../imgs/Brasil/selecao/neymar_SB.jpg",
+    posicao: "ATACANTE",
+    nome: "Neymar",
+    cidade: "Mogi das Cruzes, Brasil",
+    copas: "4ª",
+    idade: "34",
+    destaque: "Ídolo",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 021",
+ imagem: "../../imgs/Brasil/selecao/endrick_SB.jpg",
+    posicao: "ATACANTE",
+    nome: "Endrick",
+    cidade: "Taguatinga, Brasil",
+    copas: "1ª",
+    idade: "20",
+    destaque: "Promessa",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 022",
+ imagem: "../../imgs/Brasil/selecao/gabrielMartinelli_SB.jpg",
+    posicao: "ATACANTE",
+    nome: "Gabriel Martinelli",
+    cidade: "Guarulhos, Brasil",
+    copas: "2ª",
+    idade: "25",
+    destaque: "Drible",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 018",
+    imagem: "../../imgs/Brasil/selecao/mateuscunha.jpg",
+    posicao: "ATACANTE",
+    nome: "Matheus Cunha",
+    cidade: "João Pessoa, Brasil",
+    copas: "2ª",
+    idade: "27",
+    destaque: "Finalização",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 019",
+ imagem: "../../imgs/Brasil/selecao/luisHenrique_SB.jpg",
+    posicao: "ATACANTE",
+    nome: "Luiz Henrique",
+    cidade: "Petrópolis, Brasil",
+    copas: "1ª",
+    idade: "25",
+    destaque: "Velocidade",
+    cores: ["#009739", "#FEDD00", "#002776"]
+},
+
+{
+    codigo: "BRA - 020",
+ imagem: "../../imgs/Brasil/selecao/rayan_SB.jpg",
+    posicao: "ATACANTE",
+    nome: "Rayan",
+    cidade: "Rio de Janeiro, Brasil",
+    copas: "1ª",
+    idade: "19",
+    destaque: "Jovem",
+    cores: ["#009739", "#FEDD00", "#002776"]
+}
+],
+
+
+
+    mexico: [
+
+        {
+            codigo: "MEX - 009",
+            imagem: "https://tmssl.akamaized.net/images/foto/galerie/raul-jimenez-mexiko-2024-1718365314-139387.jpg",
+            posicao: "ATACANTE",
+            nome: "Raúl Jiménez",
+            cidade: "Hidalgo, México",
+            copas: "4ª",
+            idade: "34",
+            destaque: "Veterano",
+            cores: ["#006847", "#FFFFFF", "#CE1126"]
+        },
+
+        {
+            codigo: "MEX - 004",
+            imagem: "https://tmssl.akamaized.net/images/foto/galerie/edson-alvarez-mexiko-2024-1718365188-139385.jpg",
+            posicao: "VOLANTE",
+            nome: "Edson Álvarez",
+            cidade: "Tlalnepantla, México",
+            copas: "3ª",
+            idade: "28",
+            destaque: "Líder",
+            cores: ["#006847", "#FFFFFF", "#CE1126"]
+        }
+
+    ],
+
+
+
+
+
+
+
+
+    argentina: [
+
+        {
+            codigo: "ARG - 010",
+            imagem: "https://tmssl.akamaized.net/images/foto/galerie/lionel-messi-argentina-2024-1718366407-139389.jpg",
+            posicao: "MEIA",
+            nome: "Lionel Messi",
+            cidade: "Rosário, Argentina",
+            copas: "6ª",
+            idade: "38",
+            destaque: "Lenda",
+            cores: ["#74ACDF", "#FFFFFF", "#74ACDF"]
+        },
+
+        {
+            codigo: "ARG - 009",
+            imagem: "https://tmssl.akamaized.net/images/foto/galerie/julian-alvarez-argentinien-2024-1718366534-139391.jpg",
+            posicao: "ATACANTE",
+            nome: "Julián Álvarez",
+            cidade: "Córdoba, Argentina",
+            copas: "2ª",
+            idade: "25",
+            destaque: "Finalizador",
+            cores: ["#74ACDF", "#FFFFFF", "#74ACDF"]
+        }
+
+    ],
+
+
+
+
+
+
+
+
+    franca: [
+
+        {
+            codigo: "FRA - 010",
+            imagem: "https://tmssl.akamaized.net/images/foto/galerie/kylian-mbappe-frankreich-2024-1718367028-139394.jpg",
+            posicao: "ATACANTE",
+            nome: "Mbappé",
+            cidade: "Paris, França",
+            copas: "3ª",
+            idade: "27",
+            destaque: "Velocidade",
+            cores: ["#0055A4", "#FFFFFF", "#EF4135"]
+        }
+
+    ],
+
+
+
+
+
+
+
+
+    alemanha: [
+
+        {
+            codigo: "GER - 008",
+            imagem: "https://tmssl.akamaized.net/images/foto/galerie/jamal-musiala-deutschland-2024-1718367338-139397.jpg",
+            posicao: "MEIA",
+            nome: "Musiala",
+            cidade: "Stuttgart, Alemanha",
+            copas: "2ª",
+            idade: "23",
+            destaque: "Drible",
+            cores: ["#000000", "#DD0000", "#FFCE00"]
+        }
+
+    ]
+
+}
+function atualizarCoresPais(pais){
+
+    const coresPais = selecoes[pais][0].cores
+    const corPrincipal = coresPais[0]
+
+    const linhaTopo = document.getElementById("linhaTopo")
+
+    if(linhaTopo){
+
+        linhaTopo.style.background = `
+        linear-gradient(
+            to right,
+            ${coresPais[0]} 0%,
+            ${coresPais[0]} 33%,
+            ${coresPais[1]} 33%,
+            ${coresPais[1]} 66%,
+            ${coresPais[2]} 66%,
+            ${coresPais[2]} 100%
+        )
+        `
+    }
+
+    document.querySelectorAll(".cor-pais").forEach(elemento => {
+        elemento.style.color = corPrincipal
+    })
+
+    document.querySelectorAll(".ano").forEach(elemento => {
+        elemento.style.color = corPrincipal
+    })
+}
+
+function renderizarJogadores(pais){
+
+    const container = document.getElementById("jogadores")
+
+    if(!container) return
+
+    container.innerHTML = ""
+
+    atualizarCoresPais(pais)
+
+    selecoes[pais].forEach(jogador => {
+
+        container.innerHTML += `
+
+        <div class="card">
+
+            <div class="topo">
+
+                <p class="codigo">
+                    ${jogador.codigo}
+                </p>
+
+                <div class="bandeira">
+
+                    ${jogador.cores.map(cor => `
+                        <div class="cor" style="background:${cor}"></div>
+                    `).join("")}
+
+                </div>
+
+            </div>
+
+            <div class="imagem">
+                <img src="${jogador.imagem}" alt="${jogador.nome}">
+            </div>
+
+            <p class="posicao">
+                ${jogador.posicao}
+            </p>
+
+            <p class="nome">
+                ${jogador.nome}
+            </p>
+
+            <p class="cidade">
+                ${jogador.cidade}
+            </p>
+
+            <div class="linha-card"></div>
+
+            <div class="infos">
+
+                <div class="info">
+                    <p class="numero">${jogador.copas}</p>
+                    <p class="texto">Copa</p>
+                </div>
+
+                <div class="info">
+                    <p class="numero">${jogador.idade}</p>
+                    <p class="texto">Anos</p>
+                </div>
+
+                <div class="info">
+                    <p class="numero estrela">★</p>
+                    <p class="texto">${jogador.destaque}</p>
+                </div>
+
+            </div>
+
+            <div class="rodape">
+
+                <p>
+                    ${jogador.codigo}
+                </p>
+
+                <p>
+                    Panini
+                </p>
+
+            </div>
+
+        </div>
+
+        `
+    })
+}
+
+function scrollJogadores(direcao){
+
+    const container = document.getElementById("jogadores")
+
+    container.scrollBy({
+        left: direcao * 260,
+        behavior: "smooth"
+    })
+}
+
+
+
